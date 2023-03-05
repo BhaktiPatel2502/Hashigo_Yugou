675955b862483b680b547702847313db9e3c4f0c
BhaktiPatel2502 <20it005@charusat.edu.in>
2023-03-05 11:30:00 +0530

    testcase

---
+++ demo/lib/demo.dart
@@ -0,0 +1,209 @@
+import "dart:math";
+
+const maxRows = 7;
+const maxColumns = 7;
+const alphabet = 26;
+var alphabets =
+    List.generate(alphabet, (index) => String.fromCharCode(index + 65));
+var listOfElement = List.generate(maxRows, (_) => List.filled(maxRows, ''));
+Set resultSet = {"Y", "S", "AA"};
+
+var swipeSelection = [
+  clickFunctionSwipeDownColumns(2),
+  clickFunctionSwipeDownColumns(3),
+  clickFunctionSwipeDownColumns(4),
+  clickFunctionSwipeUpColumns(2),
+  clickFunctionSwipeUpColumns(2),
+  clickFunctionSwipeUpColumns(2),
+  clickFunctionSwipeUpColumns(3),
+  clickFunctionSwipeUpColumns(4),
+  clickFunctionSwipeLeftRows(2),
+  clickFunctionSwipeLeftRows(3),
+  clickFunctionSwipeLeftRows(4),
+  clickFunctionSwipeRightRows(2),
+  clickFunctionSwipeRightRows(3),
+  clickFunctionSwipeRightRows(4)
+];
+
+Random rnd =  Random();
+
+void main() {
+  initialList();
+
+  print("initial Matrix of Element: ");
+  for (int i = 0; i < maxRows; i++) {
+    print(
+        ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
+  }
+
+  screenDisplayGrid();
+
+  swipeSelection[rnd.nextInt(swipeSelection.length)];
+}
+
+// Add element into a List
+
+void initialList() {
+  var twoLetterCombinations = List.generate(alphabet * alphabet, (index) {
+    int firstLetterIndex = (index ~/ alphabet);
+    int secondLetterIndex = (index % alphabet);
+
+    return '${alphabets[firstLetterIndex]}${alphabets[secondLetterIndex]}';
+  });
+
+  int index = 0;
+  for (int i = 0; i < maxRows; i++) {
+    for (int j = 0; j < maxRows; j++) {
+      if (index < alphabet) {
+        listOfElement[i][j] = alphabets[index];
+      } else {
+        listOfElement[i][j] = twoLetterCombinations[index - alphabet];
+      }
+      index++;
+    }
+  }
+}
+
+// Swape Down Columns and test their old value with new value
+
+void clickFunctionSwipeDownColumns(int index) {
+ 
+  listOfElement = swipeDownFunction(listOfElement, maxColumns, index);
+  print("-------------------------------------------------");
+  print("Swipe after ${index + 1} Column Down: ");
+  
+  for (int i = 0; i < maxRows; i++) {
+    print(
+        ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
+  }
+  screenDisplayGrid();
+}
+
+List<List<String>> swipeDownFunction(
+    var listOfElement, int maxColumns, int index) {
+  var lastElement = listOfElement[6][index];
+  for (int i = maxColumns - 2; i >= 0; i--) {
+    listOfElement[i + 1][index] = listOfElement[(i) % maxColumns][index];
+  }
+  listOfElement[0][index] = lastElement;
+  return listOfElement;
+}
+
+
+
+// Swape Up Columns and test their old value with new value
+
+void clickFunctionSwipeUpColumns(int index) {
+  
+  listOfElement = swipeUpFunction(listOfElement, maxColumns, index);
+  print("-------------------------------------------------");
+  print("Swipe after ${index + 1} Column Up: ");
+  
+  for (int i = 0; i < maxRows; i++) {
+    print(
+        ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
+  }
+  screenDisplayGrid();
+}
+
+List<List<String>> swipeUpFunction(
+    var listOfElement, int maxColumns, int index) {
+  var firstElement = listOfElement[0][index];
+  for (int i = 0; i < maxRows; i++) {
+    listOfElement[i][index] = listOfElement[(i + 1) % maxColumns][index];
+  }
+  listOfElement[6][index] = firstElement;
+  return listOfElement;
+}
+
+
+
+// Swape Left Rows and test their old value with new value
+
+void clickFunctionSwipeLeftRows(int index) {
+  
+  listOfElement = swipeLeftFunction(listOfElement, maxRows, index);
+  print("-------------------------------------------------");
+  print("Swipe after ${index + 1} Row Left: ");
+ 
+  for (int i = 0; i < maxRows; i++) {
+    print(
+        ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
+  }
+  screenDisplayGrid();
+}
+
+List<List<String>> swipeLeftFunction(
+    var listOfElement, int maxRows, int index) {
+  var a = listOfElement[index][0];
+  for (int i = 0; i < maxRows; i++) {
+    listOfElement[index][i] = listOfElement[index][(i + 1) % maxRows];
+  }
+  listOfElement[index][6] = a;
+  return listOfElement;
+}
+
+
+
+// Swape Right Rows and test their old value with new value
+
+void clickFunctionSwipeRightRows(int index) {
+  
+  listOfElement = swipeRightFunction(listOfElement, maxRows, index);
+  print("-------------------------------------------------");
+  print("Swipe after ${index + 1} Row Right: ");
+  
+  for (int i = 0; i < maxRows; i++) {
+    print(
+        ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
+  }
+  screenDisplayGrid();
+}
+
+List<List<String>> swipeRightFunction(
+    var listOfElement, int maxColumns, int index) {
+  var a = listOfElement[index][6];
+  for (int i = maxRows - 2; i >= 0; i--) {
+    listOfElement[index][i + 1] = listOfElement[index][(i) % maxColumns];
+  }
+  listOfElement[index][0] = a;
+  return listOfElement;
+}
+
+
+
+void screenDisplayGrid() {
+  print(" ");
+  print("----------Grid of 3*3----------");
+  for (int i = 2; i < 5; i++) {
+    print(
+        ' ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} ');
+  }
+  matchingFunction();
+}
+
+void matchingFunction() {
+  if (listOfElement[3][2] == resultSet.elementAt(0) ||
+      listOfElement[3][2] == resultSet.elementAt(1) ||
+      listOfElement[3][2] == resultSet.elementAt(2)) {
+    print("Match Character");
+  } else {
+    print("Not Match Character");
+  }
+
+  if (listOfElement[3][3] == resultSet.elementAt(0) ||
+      listOfElement[3][3] == resultSet.elementAt(1) ||
+      listOfElement[3][3] == resultSet.elementAt(2)) {
+    print("Match Character");
+  } else {
+    print("Not Match Character");
+  }
+
+  if (listOfElement[3][4] == resultSet.elementAt(0) ||
+      listOfElement[3][4] == resultSet.elementAt(1) ||
+      listOfElement[3][4] == resultSet.elementAt(2)) {
+    print('Match Character');
+  } else {
+    print('Not Match Character');
+  }
+}
---
+++ demo/test/project_test.dart
@@ -0,0 +1,121 @@
+//generate similar code for swipedown function
+
+import 'package:demo/demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('swipeUpFunction', () {
+    var matrix = [
+      ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
+      ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
+      ['V', 'W', 'X', 'Y', 'Z', 'AA', 'AB'],
+      ['AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'],
+      ['AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP'],
+      ['AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW'],
+    ];
+
+    for (var col = 0; col < matrix[0].length; col++) {
+      test('swipe up column $col', () {
+        var expectedMatrix = List.generate(matrix.length, (row) {
+          var newRow = List.from(matrix[row]);
+          if (row == matrix.length - 1) {
+            newRow[col] = matrix[0][col];
+          } else {
+            newRow[col] = matrix[row + 1][col];
+          }
+          return newRow;
+        });
+
+        var actualMatrix = swipeUpFunction(matrix, matrix[0].length, col);
+
+        expect(actualMatrix, equals(expectedMatrix));
+      });
+    }
+  });
+
+group('swipeDownFunction', () {
+    var matrix = [      ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
+      ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
+      ['V', 'W', 'X', 'Y', 'Z', 'AA', 'AB'],
+      ['AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'],
+      ['AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP'],
+      ['AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW'],
+    ];
+
+    for (var col = 0; col < matrix[0].length; col++) {
+      test('swipe down column $col', () {
+        var expectedMatrix = List.generate(matrix.length, (row) {
+          var newRow = List.from(matrix[row]);
+          if (row == 0) {
+            newRow[col] = matrix[matrix.length - 1][col];
+          } else {
+            newRow[col] = matrix[row - 1][col];
+          }
+          return newRow;
+        });
+
+        var actualMatrix = swipeDownFunction(matrix, matrix[0].length, col);
+
+        expect(actualMatrix, equals(expectedMatrix));
+      });
+    }
+  });
+
+group('swipeLeftFunction', () {
+    var matrix = [      ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
+      ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
+      ['V', 'W', 'X', 'Y', 'Z', 'AA', 'AB'],
+      ['AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'],
+      ['AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP'],
+      ['AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW'],
+    ];
+
+    for (var row = 0; row < matrix.length; row++) {
+      test('swipe left row $row', () {
+        var expectedMatrix = List.generate(matrix.length, (i) {
+          var newRow = List.from(matrix[i]);
+          if (i == row) {
+            newRow = matrix[row].sublist(1) + [matrix[row][0]];
+          }
+          return newRow;
+        });
+
+        var actualMatrix = swipeLeftFunction(matrix, matrix.length, row);
+
+        expect(actualMatrix, equals(expectedMatrix));
+      });
+    }
+  });
+ 
+group('swipeRightFunction', () {
+    var matrix = [      ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+      ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
+      ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
+      ['V', 'W', 'X', 'Y', 'Z', 'AA', 'AB'],
+      ['AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'],
+      ['AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP'],
+      ['AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW'],
+    ];
+
+    for (var row = 0; row < matrix.length; row++) {
+      test('swipe right row $row', () {
+        var expectedMatrix = List.generate(matrix.length, (i) {
+          var newRow = List.from(matrix[i]);
+          if (i == row) {
+            newRow = [matrix[row][matrix[row].length - 1]] + matrix[row].sublist(0, matrix[row].length - 1);
+          }
+          return newRow;
+        });
+
+        var actualMatrix = swipeRightFunction(matrix, matrix.length, row);
+
+        expect(actualMatrix, equals(expectedMatrix));
+      });
+    }
+  });
+
+
+}
