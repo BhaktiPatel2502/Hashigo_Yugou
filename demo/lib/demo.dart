44ee2d6c29595f943a6701e3088efb959fce8a8a
BhaktiPatel2502 <20it005@charusat.edu.in>
2023-03-10 13:20:08 +0530

    Maximum moves code

--- demo/lib/demo.dart
+++ demo/lib/demo.dart
@@ -1,31 +1,33 @@
 import "dart:math";
 
-const maxRows = 7;
-const maxColumns = 7;
+const maxRows = 9;
+const maxColumns = 9;
 const alphabet = 26;
 var alphabets =
     List.generate(alphabet, (index) => String.fromCharCode(index + 65));
 var listOfElement = List.generate(maxRows, (_) => List.filled(maxRows, ''));
-Set resultSet = {"Y", "S", "AA"};
+Set resultSet = {"Y", "X", "AA"};
+
+int movesCounter = 0;
+int maxMoves = 5;
+bool middleRowMatched = false;
 
 var swipeSelection = [
-  clickFunctionSwipeDownColumns(2),
-  clickFunctionSwipeDownColumns(3),
-  clickFunctionSwipeDownColumns(4),
-  clickFunctionSwipeUpColumns(2),
-  clickFunctionSwipeUpColumns(2),
-  clickFunctionSwipeUpColumns(2),
-  clickFunctionSwipeUpColumns(3),
-  clickFunctionSwipeUpColumns(4),
-  clickFunctionSwipeLeftRows(2),
-  clickFunctionSwipeLeftRows(3),
-  clickFunctionSwipeLeftRows(4),
-  clickFunctionSwipeRightRows(2),
-  clickFunctionSwipeRightRows(3),
-  clickFunctionSwipeRightRows(4)
+  () => clickFunctionSwipeDownColumns(2),
+  () => clickFunctionSwipeDownColumns(3),
+  () => clickFunctionSwipeDownColumns(4),
+  () => clickFunctionSwipeUpColumns(2),
+  () => clickFunctionSwipeUpColumns(3),
+  () => clickFunctionSwipeUpColumns(4),
+  () => clickFunctionSwipeLeftRows(2),
+  () => clickFunctionSwipeLeftRows(3),
+  () => clickFunctionSwipeLeftRows(4),
+  () => clickFunctionSwipeRightRows(2),
+  () => clickFunctionSwipeRightRows(3),
+  () => clickFunctionSwipeRightRows(4)
 ];
 
-Random rnd =  Random();
+Random rnd = Random();
 
 void main() {
   initialList();
@@ -38,7 +40,19 @@ void main() {
 
   screenDisplayGrid();
 
-  swipeSelection[rnd.nextInt(swipeSelection.length)];
+  for (int i = 0; i < maxMoves; i++) {
+    swipeSelection[rnd.nextInt(swipeSelection.length)]();
+
+    if (middleRowMatched) {
+      break;
+    }
+
+    if (i == maxMoves - 1) {
+      print("Maximum ${maxMoves} moves reached");
+
+      break;
+    }
+  }
 }
 
 // Add element into a List
@@ -67,16 +81,16 @@ void initialList() {
 // Swape Down Columns and test their old value with new value
 
 void clickFunctionSwipeDownColumns(int index) {
- 
   listOfElement = swipeDownFunction(listOfElement, maxColumns, index);
   print("-------------------------------------------------");
   print("Swipe after ${index + 1} Column Down: ");
-  
+
   for (int i = 0; i < maxRows; i++) {
     print(
         ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
   }
   screenDisplayGrid();
+  resultMatchSet();
 }
 
 List<List<String>> swipeDownFunction(
@@ -89,21 +103,19 @@ List<List<String>> swipeDownFunction(
   return listOfElement;
 }
 
-
-
 // Swape Up Columns and test their old value with new value
 
 void clickFunctionSwipeUpColumns(int index) {
-  
   listOfElement = swipeUpFunction(listOfElement, maxColumns, index);
   print("-------------------------------------------------");
   print("Swipe after ${index + 1} Column Up: ");
-  
+
   for (int i = 0; i < maxRows; i++) {
     print(
         ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
   }
   screenDisplayGrid();
+  resultMatchSet();
 }
 
 List<List<String>> swipeUpFunction(
@@ -116,21 +128,19 @@ List<List<String>> swipeUpFunction(
   return listOfElement;
 }
 
-
-
 // Swape Left Rows and test their old value with new value
 
 void clickFunctionSwipeLeftRows(int index) {
-  
   listOfElement = swipeLeftFunction(listOfElement, maxRows, index);
   print("-------------------------------------------------");
   print("Swipe after ${index + 1} Row Left: ");
- 
+
   for (int i = 0; i < maxRows; i++) {
     print(
         ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
   }
   screenDisplayGrid();
+  resultMatchSet();
 }
 
 List<List<String>> swipeLeftFunction(
@@ -143,21 +153,19 @@ List<List<String>> swipeLeftFunction(
   return listOfElement;
 }
 
-
-
 // Swape Right Rows and test their old value with new value
 
 void clickFunctionSwipeRightRows(int index) {
-  
   listOfElement = swipeRightFunction(listOfElement, maxRows, index);
   print("-------------------------------------------------");
   print("Swipe after ${index + 1} Row Right: ");
-  
+
   for (int i = 0; i < maxRows; i++) {
     print(
         ' ${listOfElement[i][0]} \t ${listOfElement[i][1]} \t ${listOfElement[i][2]} \t ${listOfElement[i][3]} \t ${listOfElement[i][4]} \t ${listOfElement[i][5]} \t ${listOfElement[i][6]}  ');
   }
   screenDisplayGrid();
+  resultMatchSet();
 }
 
 List<List<String>> swipeRightFunction(
@@ -169,8 +177,6 @@ List<List<String>> swipeRightFunction(
   listOfElement[index][0] = a;
   return listOfElement;
 }
-
-
 
 void screenDisplayGrid() {
   print(" ");
@@ -205,5 +211,23 @@ void matchingFunction() {
     print('Match Character');
   } else {
     print('Not Match Character');
+  }
+}
+
+void resultMatchSet() {
+
+  if ((listOfElement[3][2] == resultSet.elementAt(0) ||
+      listOfElement[3][2] == resultSet.elementAt(1) ||
+      listOfElement[3][2] == resultSet.elementAt(2)) &&
+          (listOfElement[3][3] == resultSet.elementAt(0) ||
+      listOfElement[3][3] == resultSet.elementAt(1) ||
+      listOfElement[3][3] == resultSet.elementAt(2)) &&
+          (listOfElement[3][4] == resultSet.elementAt(0) ||
+      listOfElement[3][4] == resultSet.elementAt(1) ||
+      listOfElement[3][4] == resultSet.elementAt(2))) {
+    print("Middle row matched with result set");
+    middleRowMatched = true;
+  } else {
+    print("Middle row does not matched with result set");
   }
 }
